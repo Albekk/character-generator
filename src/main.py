b5322d3372531765e386f3f8e51063f46da4339c
Albekk <alex.bekker2004@gmail.com>
2026-09-25 20:49:21 +0300

    test+create

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def main():
+    print("================================")
+    print("      CHARACTER GENERATOR")
+    print("================================")
+    print()
+    print("Генератор персонажей для RPG")
+    print()
+
+
+if __name__ == "__main__":
+    main()
